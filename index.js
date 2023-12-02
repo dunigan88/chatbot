a180819614f3df3f51bc8afdc8e7ee08e00b2f25
Jackie Liu <jackie.anj.liu@gmail.com>
2023-12-01 21:09:41 -0800

    feat: have express serve up static front end page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import OpenAIApi from "openai"
+import OpenAIApi from 'openai'
 import express from 'express'
-import cors from "cors"
+import cors from 'cors'
+import fs from 'fs'
 
 const openai = new OpenAIApi({
     apiKey: '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'
@@ -11,6 +12,18 @@ const port = 3000
 
 app.use(express.json())
 app.use(cors())
+
+// app.get('/', (req, res) => {
+//     fs.readFile('./index.html', 'utf8', (err, html) => {
+//         if (err) {
+//             res.status(500).send('sorry, out of order')
+//         }
+
+//         res.send(html)
+//     })
+// })
+
+app.use(express.static('public'))
 
 app.post('/', async (req, res) => {
     let { messages } = req.body
